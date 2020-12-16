5c647ddf4a6431765e066e3cd08f00564ba9bc93
brice <briceguard-git@yahoo.com>
2020-12-16 11:23:14 +0000

    mv healthkit to rzutils touch

--- Package.swift
+++ Package.swift
@@ -10,10 +10,9 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "RZUtilsCore",
-            targets: ["RZUtilsCore"]
-        ),
+        .library(name: "RZUtilsCore", targets: ["RZUtilsCore" ] ),
+        .library(name: "RZUtilsUniversal", targets: ["RZUtilsUniversal" ] ),
+        .library(name: "RZUtilsSwift", targets: ["RZUtilsSwift" ] ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
