6e7096457295d8a269eca7cc7bcd28a99c3dd5c9
brice <briceguard-git@yahoo.com>
2022-08-14 15:14:39 +0100

    extension for number with unit

--- Sources/RZUtilsSwift/RZUtilsExensions.swift
+++ Sources/RZUtilsSwift/RZUtilsExensions.swift
@@ -14,3 +14,36 @@ extension GCStatsDataSerie : Sequence {
         return NSFastEnumerationIterator(self)
     }
 }
+
+extension GCNumberWithUnit : Comparable {
+    
+    public static func < (lhs: GCNumberWithUnit, rhs: GCNumberWithUnit) -> Bool {
+        return lhs.compare(rhs) == ComparisonResult.orderedAscending
+    }
+}
+
+extension GCNumberWithUnit {
+    enum GCNumberWithUnitError : Error {
+        case incompatibleUnit
+    }
+    public static func +(lhs: GCNumberWithUnit, rhs: GCNumberWithUnit) throws -> GCNumberWithUnit {
+        if let rv = lhs.add(rhs, weight: 1.0) {
+            return rv
+        }
+        throw GCNumberWithUnitError.incompatibleUnit
+    }
+    
+    public static func -(lhs: GCNumberWithUnit, rhs: GCNumberWithUnit) throws -> GCNumberWithUnit {
+        if let rv = lhs.add(rhs, weight: -1.0) {
+            return rv
+        }
+        throw GCNumberWithUnitError.incompatibleUnit
+    }
+    
+    public static prefix func -(nu: GCNumberWithUnit) throws -> GCNumberWithUnit {
+        if let rv = nu.numberWithUnitMultiplied(by: -1.0) {
+            return rv
+        }
+        throw GCNumberWithUnitError.incompatibleUnit
+    }
+}
